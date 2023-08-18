0c5eda57ae70ec2a76ace8b678f5a9affca1a121
DanielVrd <135719204+DanielVrd@users.noreply.github.com>
2023-08-18 16:14:26 -0300

    Novo envio da tarefa Módulo M08

---
+++ TAREFA-MOD-08/src/br/com/daniel/verdi/MediaAritmetica.java
@@ -0,0 +1,18 @@
+public class MediaAritmetica {
+
+    public static void main(String[]args){
+        mediaAritmetica();
+        System.out.println("Obrigado pelo seu tempo professor, um bom dia para o senhor.");
+
+    }
+
+    private static void mediaAritmetica() {
+        double nota1 = 9.5;
+        int nota2 = 8;
+        double nota3 = 5.5;
+        int nota4 = 3;
+        double media = (nota1 + nota2 + nota3 + nota4)/4;
+        System.out.println("media: " + media);
+
+    }
+}
